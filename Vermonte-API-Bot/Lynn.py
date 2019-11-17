3f0f9e1ba0fc9e366562284e72c2763d4275c408
Paul Day <Dire@vmi214495.contaboserver.net>
2019-11-17 16:14:01 +0100

    adding new files

---
+++ Vermonte-API-Bot/Lynn.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext import commands
+import traceback
+import config
+import os
+import logging
+
+errors = ""
+bot = commands.Bot(command_prefix=config.get_prefix, description=config.description)
+
+if __name__ == '__main__':
+    for extension in [f.replace('.py', '') for f in os.listdir("extensions") if os.path.isfile(os.path.join("extensions", f))]:
+        try:
+            bot.load_extension(config.cogDir + '.' + extension)
+        except (discord.ClientException, ModuleNotFoundError):
+            logging.error(f'Failed to load extension {extension}.', exc_info=True)
+
+    @bot.event
+    async def on_ready():
+        logging.info(f'Logged in as: {bot.user.name} - {bot.user.id}')
+        logging.info(f'Discord.py version: {discord.__version__}')
+
+    @bot.event
+    async def on_message(message):
+        if message.author.bot:
+            return
+        ctx = await bot.get_context(message)
+        if ctx.valid:
+            await message.add_reaction("\N{HOURGLASS}")
+            await bot.process_commands(message)
+            try:
+                await message.remove_reaction("\N{HOURGLASS}", bot.user)
+            except:
+                pass
+
+    bot.run(config.token, bot=True, reconnect=True)
